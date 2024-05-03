f09d208f0b9886b907ea7dc0c1aaf3a944ef497d
Gourav More <gourav_m@tekditechnologies.com>
2024-05-03 11:01:17 +0530

    Login page integration

--- src/views/LoginPage/LoginPage.jsx
+++ src/views/LoginPage/LoginPage.jsx
@@ -1,18 +1,35 @@
 import React, { useState } from 'react';
 import './LoginPage.css'; // Import the CSS file for styling
+import axios from 'axios';
+import config from "../../utils/urlConstants.json";
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
+    localStorage.clear();
     e.preventDefault();
     // Add your login logic here
     console.log('Username:', username);
     console.log('Password:', password);
+    const usernameDetails = await axios.get(
+        `${process.env.REACT_APP_LEARNER_AI_ORCHESTRATION_HOST}/${config.URLS.GET_VIRTUAL_ID}?username=${username}&password=${password}`
+        );
     // For demonstration purposes, let's assume the login is successful
     // and redirect to '/discover-start'
-    window.location.href = '/discover-start';
+
+    if (usernameDetails?.data.virtualID){
+        localStorage.setItem("profileName", username);
+        localStorage.setItem(
+            "virtualId",
+            usernameDetails?.data?.virtualID
+          );
+        window.location.href = '/discover-start';
+    }else{
+        alert("Enter correct username and password")
+    }
+    
   };
 
   return (
