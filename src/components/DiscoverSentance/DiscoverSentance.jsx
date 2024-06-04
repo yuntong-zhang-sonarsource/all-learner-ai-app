6eba68890271fdb81645925f791f32f99a25cb9d
deepali chavhan <deechavhan098@gmail.com>
2024-06-04 12:54:34 +0530

    Task #0000 fix: pagination API is failing

--- src/components/DiscoverSentance/DiscoverSentance.jsx
+++ src/components/DiscoverSentance/DiscoverSentance.jsx
@@ -190,7 +190,7 @@ const SpeakSentenceComponent = () => {
             (elem) => elem.category === "Sentence"
           );
           const resSentencesPagination = await axios.get(
-            `${process.env.REACT_APP_CONTENT_SERVICE_APP_HOST}/${config.URLS.GET_PAGINATION}?page=1&limit=5&collectionId=${sentences?.[newSentencePassedCounter]?.content?.[0]?.collectionId}`
+            `${process.env.REACT_APP_CONTENT_SERVICE_APP_HOST}/${config.URLS.GET_PAGINATION}?page=1&limit=5&collectionId=${sentences?.[newSentencePassedCounter]?.collectionId}`
           );
           setCurrentContentType("Sentence");
           setTotalSyllableCount(resSentencesPagination?.data?.totalSyllableCount);
@@ -214,7 +214,7 @@ const SpeakSentenceComponent = () => {
             (elem) => elem.category === "Word"
           );
           const resWordsPagination = await axios.get(
-            `${process.env.REACT_APP_CONTENT_SERVICE_APP_HOST}/${config.URLS.GET_PAGINATION}?page=1&limit=5&collectionId=${words?.content?.[0]?.collectionId}`
+            `${process.env.REACT_APP_CONTENT_SERVICE_APP_HOST}/${config.URLS.GET_PAGINATION}?page=1&limit=5&collectionId=${words?.collectionId}`
           );
           setCurrentContentType("Word");
           setTotalSyllableCount(resWordsPagination?.data?.totalSyllableCount);
