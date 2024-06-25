e5a50f85e3689fce4ac1e7be3fdeeea46c54f9de
deepali chavhan <deechavhan098@gmail.com>
2024-06-25 18:48:42 +0530

    Task #217402 fix: Husky Implementation and Integration in React

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+// import '@testing-library/jest-dom/extend-expect';
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
+const mockStore = configureStore([]);
+const initialState = {
+  snackbar: {
+    open: false,
+    message: "",
+    type: "",
+  },
+  user: {
+    virtulId: null,
+  },
+};
+const store = mockStore(initialState);
+
+test("renders learn react link", () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,26 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+Object.defineProperty(URL, "createObjectURL", {
+  writable: true,
+  value: jest.fn(),
+});
+
+class WorkerMock {
+  constructor(stringUrl) {
+    this.url = stringUrl;
+    this.onmessage = null;
+  }
+
+  postMessage(msg) {
+    if (this.onmessage) {
+      this.onmessage({ data: msg });
+    }
+  }
+
+  terminate() {}
+}
+
+window.Worker = WorkerMock;
