9e76ae11aff7872a78883b058229267859ea7acf
Sudeep Ratnaparkhe <sudeeppr1998@gmail.com>
2024-07-15 17:43:20 +0530

    Check added for Module init

--- public/whisper.wasm/initWhisper.js
+++ public/whisper.wasm/initWhisper.js
@@ -1,5 +1,8 @@
 const loadWhisperModule = async () => {
     try {
+      if (typeof Module === 'undefined') {
+        throw new Error('Module is not defined');
+      }
       window.whisperModule = Module;
     } catch (error) {
       console.error('Failed to load WASM module', error);
