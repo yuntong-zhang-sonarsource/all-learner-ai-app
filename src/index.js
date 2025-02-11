0a81939bfe4c4a74d311f30fb1516c0377d3add5
Ajinkya Pande <ajinkya.pande@tekditechnologies.com>
2025-02-11 11:18:03 +0530

    Issueid #234841 feat:Monitor Session Expiry and Redirect to Login

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import './assets/styles/index.scss';
-import store from './store/configureStore';
-import './index.css';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./assets/styles/index.scss";
+import store from "./store/configureStore";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
 render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root'),
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
